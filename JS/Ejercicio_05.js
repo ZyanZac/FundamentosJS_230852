f915e8f5c447fd1bec3cb1d73e1cdf346da5e8f8
Zyanya Ahuachtli Zacatenco Pedroza <230852@utxicotepec.edu.mx>
2024-10-16 22:18:58 -0600

    Práctica 07
    Adición de los puntos 2 y 3 del archivo Ejercicio_05.js

--- JS/Ejercicio_05.js
+++ JS/Ejercicio_05.js
@@ -1,16 +1,20 @@
 //
 
 //Estilización de los mensajes de salida
-const bg="linear-gradient(11deg, rgba(0,199,36,1) 0%, rgba(129,255,26,1) 33%, rgba(191,180,25,1) 86%)";
+const bg="linear-gradient(11deg, rgba(0,199,36,1) 0%, rgba(129,255,26,1) 33%, rgba(191,255,26,1) 86%)";
 const style_console=`background: ${bg}; color: white; border-radius: 6px; padding: 4px; font-size: 1.0rem; font-weight: bold`
 
 //Personalización de las Salidas a Consola
-console.warn("Práctica 07: Arreglos en Java Script");
+console.warn("Práctica 07: Repaso de Ciclos y Sentencias de Control");
 
-console.log("%c1.- Si/Entonces... (IF)", style_console);
+console.log("%c1.-  Condicionales - Si/Entonces... (IF)", style_console);
 //Le indica al programa que hacer o no en base a una prueba lógica de verdadero o falso
 
+//Cuando se declare una fecha con enteros, 0 es enero
 let fechaActual=new Date();
+//let fechaActual=new Date(2024, 7, 5); 
+//let fechaActual=new Date(2025, 4, 30);
+//let fechaActual=new Date(2024, 1, 6);
 console.log(`Hola, la fecha de hoy es: ${fechaActual.toString()}`)
 
 //Y si la necesitamos en formato local:
@@ -47,9 +51,89 @@ let inicioPrimavera=new Date(anio, 2, 21);
 let inicioVerano=new Date(anio, 5, 21);
 let inicioOtonio=new Date(anio, 8, 23);
 let inicioInvierno=new Date(anio, 11, 21);
-
+let estacion;
 let horarioVerano=false;
-if(fechaActual>=inicioPrimavera || fechaActual<inicioVerano){
+
+if(fechaActual>=inicioPrimavera && fechaActual<inicioVerano){
     console.log(`Estamos en PRIMAVERA`);
+    console.log("Inicia la floración de muchas plantas.")
+    console.log("Los días son más largos y las noches más cortas.")
+    console.log("Muchos animales despiertan de la hibernación.")
+    estacion="Primavera";
+    horarioVerano=true;
+} else if(fechaActual>=inicioVerano&&fechaActual<inicioOtonio){
+    console.log("Estamos en VERANO");
+    console.log("En esta temporada abundan los días soleados y calurosos.")
+    console.log("En esta temporada el índice de turismo vacacional sube.")
+    console.log("Mucha gente busca realizar actividades al aire.")
+    estacion="Verano";
+    horarioVerano=true;
+}else if(fechaActual>=inicioOtonio&&fechaActual<inicioInvierno){
+    console.log("Estamos en OTOÑO")
+    console.log("Los árboles suelen cambiar de follaje.")
+    console.log("Se registrarán temperaturas más templadas.")
+    console.log("Los animales comienzan con la recolección de alimento y preparan sus espacios para la hibernación, incluso algunas aves migran.")
+    estacion="Otoño";
+    horarioVerano=false;
+} else {
+    console.log("Estamos en INVIERNO")
+    console.log("En esta temporada los días son más cortos y las noches más largas.")
+    console.log("En algunas regiones suele nevar.")
+    console.log("Dado las bajas temperaturas, se recomienda abrigarse.")
+    estacion="Invierno";
+    horarioVerano=false;
 }
+
+
+
+console.log("%c2.- Operador Ternario (validación?cumple:no_cumple)", style_console);
+//En JavaScript existe una operación simplificada que valida si una condición se cumple o no, y que hacer en cada caso.
+
+const edadPersona=18;
+const mayoriaEdadMX=18;
+const mayoriaEdadUS=21;
+let evaluarMayoriaEdad=(edad)=>
+    edad>=18?"Eres mayor de edad.":"No eres mayor de edad."
+
+console.log("Evaluando la mayoría de edad de una persona: ");
+console.log(evaluarMayoriaEdad(edadPersona));
+
+//Sin embargo tenemos que considerar que la mayoría de edad varia en cada país por cuestiones legales, por lo que debemos considerar un segundo parámetro en la evaluación.
+
+evaluarMayoriaEdad=(edad, pais)=>
+    (edad>=18 && pais=="MX")?`En ${pais} eres mayor de edad.`:`En ${pais} NO eres mayor de edad.`
+
+
+console.log("Evaluando la mayoría de edad de una persona en Estados Unidos")
+console.log(evaluarMayoriaEdad(edadPersona, "USA"))
+
+
+
+
+console.log("%c3.- SWITCH - CASE (Elección por valor definido)", style_console);
+
+//Calculando tu generación en base a tu edad
+let anioNacimiento;
+let asignaGeneracion=(anioNacimiento)=>{
+
+    switch(true){
+        case(anioNacimiento<1968):
+        return "Baby Boomer";
+
+        case(anioNacimiento>1968 && anioNacimiento<=1980):
+        return "Generación X";
+
+        case(anioNacimiento>1980 && anioNacimiento<=1994):
+        return "Generación Y/Millennial";
+
+        case(anioNacimiento>1994 && anioNacimiento<=2009):
+        return "Generación Z/Centennial";
+
+        case(anioNacimiento>2009):
+        return "Genración Alfa";
+    }
+}
+
+console.log(`Dado que nací en el año 1999 soy de la generación ${asignaGeneracion(1999)}`)
+
 
