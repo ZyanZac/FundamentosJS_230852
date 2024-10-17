776cc72499a5239fc954c59ef28f81675db6d6f3
Zyanya Ahuachtli Zacatenco Pedroza <230852@utxicotepec.edu.mx>
2024-10-16 22:25:35 -0600

    Práctica 07
    Adición de los puntos 8, 9, 10, 11 y 12 del archivo Ejercicio_05.js

--- JS/Ejercicio_04.js
+++ JS/Ejercicio_04.js
@@ -284,6 +284,13 @@ console.table(signosZodiacales);
 //Que podemos hacer si necesitamos el mismo arreglo pero ahora con todos sus elementos con letras MAYÚSCULAS
 console.table(signosZodiacales.map(signoZodiacal=>signoZodiacal.toUpperCase()));
 
+
+
+
+console.log("%c13.- Reducción de Elementos de un arreglo - REDUCE", style_console);
+//Reducción de elementos de un arreglo, se usa cuando debemos hacer operaciones matemáticas o cuantitátivas a un arreglo, como obtener totales, la idea es reducir la lista a un valor más simplificado.
+
+
 //Reducción de elementos de un arreglo, se usa cuando debemos hacer operaciones matemáticas o cuantitátivas a un arreglo, como obtener totales, la idea es reducir la lista a un valor más simplificado.
 
 const costosListaCompras=[15, 52.50, 16.90, 32.50, 28, 105, 45.2, 94.10]
